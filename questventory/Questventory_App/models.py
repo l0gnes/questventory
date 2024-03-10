e82a48a17cbb28a59eb780dc0f4cccc324da8e1f
TheBigCoded <52118573+TheBigCoded@users.noreply.github.com>
2024-03-10 18:36:06 -0300

    Updated Models.py, added DB model for games
    Updated Models.py, added DB model for games

--- questventory/Questventory_App/models.py
+++ questventory/Questventory_App/models.py
@@ -1,26 +1,34 @@
 from django.db import models
-from django.utils import timezone
-from django.utils.timezone import now
-import datetime
 
-# Model for how transactions are recorded in the database.
-class Transaction(models.Model):
-    TRANSACTION_CHOICES = [
-        ('deposit', 'Deposit'),
-        ('withdrawal', 'Withdrawal'),
-    ]
+# Database model for consoles
+class Console(models.Model):
+    name = models.CharField(max_length=100)
+    manufacturer = models.CharField(max_length=100)
 
-    TRANSACTION_CATEGORIES = [
-        ('food', 'Food'),
-        ('recreation and entertainment', 'Recreation'),
-        ('fuel', 'Fuel'),
-        ('rent', 'Rent'),
-        ('miscellaneous', 'Miscellaneous'),
-        ('paycheck', 'Paycheck *For Deposits*'),
-        ('other', 'Other *For Deposits*'),
-    ]
-    
-    transaction_type = models.CharField(max_length=12, choices=TRANSACTION_CHOICES)
-    transaction_category = models.CharField(max_length=30, choices=TRANSACTION_CATEGORIES)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return self.name
+
+class Developer(models.Model):
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Database model for Genre table
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+# Database model for games, using developers as a foreign key.
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    release_date = models.DateField()
+    developer = models.ForeignKey(Developer, on_delete=models.CASCADE)
+    consoles = models.ManyToManyField(Console)
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self):
+        return self.title
