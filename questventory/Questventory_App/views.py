b0800b645cdad3afd5ca29133cfd807bf8083058
l0gnes <loonargt@gmail.com>
2024-03-10 20:28:04 -0300

    add inventory page

--- questventory/Questventory_App/views.py
+++ questventory/Questventory_App/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.template.loader import get_template
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def inventory(request):
+    return render(request, 'inventory.html')
